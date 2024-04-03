e15b7ff7fb0765172af9e22e9dea3e3994d2a558
Charlie Sergeant <theedgepredictor@gmail.com>
2024-04-03 17:48:53 -0400

    remove num classes arg and add dataset to list of datasets

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -3,7 +3,7 @@ import torch.utils.data
 import torchvision
 
 from .coco import build as build_coco
-from .pollen import build as build_face
+from .pollen import build as build_pollen
 
 
 def get_coco_api_from_dataset(dataset):
@@ -19,8 +19,8 @@ def get_coco_api_from_dataset(dataset):
 def build_dataset(image_set, args):
     if args.dataset_file == 'coco':
         return build_coco(image_set, args)
-    if args.dataset_file == 'face':
-        return build_face(image_set, args)
+    if args.dataset_file == 'pollen':
+        return build_pollen(image_set, args)
     if args.dataset_file == 'coco_panoptic':
         # to avoid making panopticapi required for coco
         from .coco_panoptic import build as build_coco_panoptic
